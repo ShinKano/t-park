7cb7341baf7a575193da2058044f33f33c010d0e
Shinnosuke Kano <kano.s@impl.co.jp>
2020-03-06 09:04:49 +0900

    add splash

--- app/src/main/java/com/example/t_park/MainActivity.java
+++ app/src/main/java/com/example/t_park/MainActivity.java
@@ -20,6 +20,7 @@ public class MainActivity extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        setTheme(R.style.AppTheme);
         setContentView(R.layout.activity_main);
 
         // 条件に合ったFragmentを設定
